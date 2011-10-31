7fcb1659ffc5ac8948a671a60197e3c63d9542fa
Ayende Rahien <ayende@ayende.com>
2011-10-31 10:59:25 -0400

    First entity defined & mapped

---
+++ Course/Models/Part.cs
@@ -0,0 +1,10 @@
+﻿namespace Course.Models
+{
+	public class Part
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string Title { get; set; }
+		public virtual string PartNumber { get; set; }
+	}
+}
