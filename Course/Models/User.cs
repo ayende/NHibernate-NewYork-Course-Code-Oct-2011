50f0ff29c6f8f55a342ca317a8b00367116869cd
Ayende Rahien <ayende@ayende.com>
2011-11-01 12:17:06 -0400

    Work & Home addresses

--- Course/Models/User.cs
+++ Course/Models/User.cs
@@ -9,6 +9,16 @@ namespace Course.Models
 		public virtual string Email { get; set; }
 		public virtual ICollection<Group> Groups { get; set; }
 		public virtual ICollection<Part> Parts { get; set; }
+
+		public virtual string WorkAddressLine1 { get; set; }
+		public virtual string WorkAddressLine2 { get; set; }
+		public virtual string WorkAddressState { get; set; }
+		public virtual string WorkAddressCity { get; set; }
+
+		public virtual string HomeAddressLine1 { get; set; }
+		public virtual string HomeAddressLine2 { get; set; }
+		public virtual string HomeAddressState { get; set; }
+		public virtual string HomeAddressCity { get; set; }
 	}
 
 	public class Admin : User
